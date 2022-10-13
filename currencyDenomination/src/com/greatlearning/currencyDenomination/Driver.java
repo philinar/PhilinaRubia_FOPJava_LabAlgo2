0192c2f94c5686600d89029887d1935007e7f86f
philinar <113802131+philinar@users.noreply.github.com>
2022-10-13 15:15:41 +0530

    Update Driver.java

--- currencyDenomination/src/com/greatlearning/currencyDenomination/Driver.java
+++ currencyDenomination/src/com/greatlearning/currencyDenomination/Driver.java
@@ -21,19 +21,21 @@ public class Driver {
 		array = mSort.sort(array);
 		
 		//logic for currency denomination
-
-		for (int i = 0; i < size; i++) {
+                for (int i = 0; i < size; i++) {
 			if (amtPayable >= array[i]) {
 				count[i] = amtPayable / array[i];
 				amtPayable = amtPayable - count[i] * array[i];
 			}
 		}
-		if (amtPayable > 0) {
-			System.out.println("Amount can not be printed with the given Denominations.");
-		} else {
-			System.out.println("Your payment approach in order to give min No. notes will be:");
-			for (int j = 0; j < size; j++) {
-				System.out.println(array[j] + ":" + count[j]);
+		// output
+		if (amtPayable == 0) {
+			System.out.println("Your payment approach in order to give min no of notes will be:");
+		}
+		for (int i = 0; i < size; i++) {
+			if (count[i] > 0) {
+				System.out.println(array[i] + " : " + count[i]);
+			} else {
+				System.out.println("Amount cannot be printed with the given denominations");
 			}
 		}
 	}
